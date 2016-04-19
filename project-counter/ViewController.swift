a8ce13681b6d14ce2517038deb6cfb934c9c6e6e
reyes <machikoreo@gmail.com>
2016-04-19 09:10:37 +0800

    initial

--- project-counter/ViewController.swift
+++ project-counter/ViewController.swift
@@ -19,7 +19,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // 宣告
+    @IBOutlet weak var digitalLabel: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var resetBtn: UIButton!
+//    @IBOutlet weak var holdBtn: UIButton!
+    
+    // 動作
+    @IBAction func addCounter(sender: AnyObject) {
+        var tmpVal = Int(digitalLabel.text!)
+//        print(tmpVal!)
+        tmpVal = tmpVal! + 1
+        digitalLabel.text = "\(tmpVal!)"
+//        print(String(tmpVal!))
+        
+    }
+    
+    @IBAction func resetCounter(sender: AnyObject) {
+        digitalLabel.text = String(0)
+    }
+    
+    
 }
 
